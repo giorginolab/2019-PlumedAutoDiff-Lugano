055d650cbd973f2bd6e5a1c262f170f63361722a
Toni <toni.giorgino@gmail.com>
2019-06-13 14:16:42 +0200

    comment

--- stan_lambda.cpp
+++ stan_lambda.cpp
@@ -20,37 +20,39 @@ using std::vector;
 
 
 
-/* The argument is a const Matrix<T, Dynamic, 1>& theta 
-   we need to extract the type T to do math on it. */
-
-auto curvature_lambda = [] (auto x) {
-    typedef typename decltype(x)::Scalar T;
-
-    // Reshape for convenience
-    Matrix<T, 3, 1> a_, o_, b_;
-    a_ << x(0) , x(1) , x(2);
-    o_ << x(3) , x(4) , x(5);
-    b_ << x(6) , x(7) , x(8);
-
-    Matrix<T, 3, 1> xx, yy, x_y;
-    xx = a_-o_;
-    yy = b_-o_;
-    x_y = a_-b_;
-
-    auto cos2_a = pow(xx.dot(yy),2.0) / xx.dot(xx) / yy.dot(yy);
-    auto sin2_a = 1.0 - cos2_a;
-
-    auto radius2 = x_y.dot(x_y) / sin2_a / 4.0;
-    auto radius = sqrt(radius2);
-
-    return(radius);
-};
-
-
-
 
 
 void grad_curvature(Matrix<double,Dynamic,1> x, double &fx, Matrix<double,Dynamic,1> &grad_fx ) {
+
+    bool inverse=false;
+    
+    auto curvature_lambda = [inverse] (auto x) {
+/* The argument is a const Matrix<T, Dynamic, 1>& theta we may need to
+   extract the type T (e.g. if we want to make compatible Eigen
+   objects). Otherwise "auto" may just do it. */
+	typedef typename decltype(x)::Scalar T;
+
+	// Reshape for convenience
+	Matrix<T, 3, 1> a_, o_, b_;
+	a_ << x(0) , x(1) , x(2);
+	o_ << x(3) , x(4) , x(5);
+	b_ << x(6) , x(7) , x(8);
+
+	Matrix<T, 3, 1> xx, yy, x_y;
+	xx = a_-o_;
+	yy = b_-o_;
+	x_y = a_-b_;
+
+	auto cos2_a = pow(xx.dot(yy),2.0) / xx.dot(xx) / yy.dot(yy);
+	auto sin2_a = 1.0 - cos2_a;
+
+	auto radius2 = x_y.dot(x_y) / sin2_a / 4.0;
+	auto radius = sqrt(radius2);
+
+	return(inverse? 1.0/radius : radius);
+    };
+
+
     // curvature_class f;
     // Matrix<double,Dynamic,1> x=xx;  // Where to evaluate f and grad f
     // double fx;                       // The value of f(x)
@@ -70,8 +72,7 @@ void test_curvature() {
 
     double radius;
     Matrix<double,Dynamic,1> grad_fx;
-    // grad_curvature(x,radius,grad_fx);
-    stan::math::gradient(curvature_lambda,x,fx,grad_fx);
+    grad_curvature(x,radius,grad_fx);
     
     std::cout << "Radius: " << radius << std::endl;
 
